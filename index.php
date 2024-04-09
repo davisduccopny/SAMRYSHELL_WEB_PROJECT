bdaa356cd9494e1b3ddf12ab60fa7e09821f6cfc
Hoang Xuan Quoc <2156210125@hcmussh.edu.vn>
2024-04-10 00:30:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -179,7 +179,19 @@ $products= $productModel->showProduct();
 <!--== Header Area End ==-->
 
 <!--== Search Box Area Start ==-->
-<?php include('./main/search_box.php'); ?>
+<div class="body-popup-modal-area">
+    <span class="modal-close"><img src="assets/img/cancel.png" alt="Close" class="img-fluid"/></span>
+    <div class="modal-container d-flex">
+        <div class="search-box-area">
+            <div class="search-box-form">
+                <form action="#" method="post">
+                    <input type="search" placeholder="type keyword and hit enter"/>
+                    <button class="btn" type="button"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <!--== Search Box Area End ==-->
 
 <!--== Banner // Slider Area Start ==-->
@@ -1061,13 +1073,165 @@ $products= $productModel->showProduct();
 <!--== Newsletter Area End ==-->
 
 <!-- Footer Area Start -->
-<?php include('./main/footer.php'); ?>
+<footer id="footer-area">
+    <!-- Footer Call to Action Start -->
+    <div class="footer-callto-action">
+        <div class="ruby-container">
+            <div class="callto-action-wrapper">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <!-- Single Call-to Action Start -->
+                        <div class="single-callto-action d-flex">
+                            <figure class="callto-thumb">
+                                <img src="assets/img/air-plane.png" alt="WorldWide Shipping"/>
+                            </figure>
+                            <div class="callto-info">
+                                <h2>Free Shipping Worldwide</h2>
+                                <p>On order over $150 - 7 days a week</p>
+                            </div>
+                        </div>
+                        <!-- Single Call-to Action End -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <!-- Single Call-to Action Start -->
+                        <div class="single-callto-action d-flex">
+                            <figure class="callto-thumb">
+                                <img src="assets/img/support.png" alt="Support"/>
+                            </figure>
+                            <div class="callto-info">
+                                <h2>24/7 CUSTOMER SERVICE</h2>
+                                <p>Call us 24/7 at 000 - 123 - 456k</p>
+                            </div>
+                        </div>
+                        <!-- Single Call-to Action End -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <!-- Single Call-to Action Start -->
+                        <div class="single-callto-action d-flex">
+                            <figure class="callto-thumb">
+                                <img src="assets/img/money-back.png" alt="Money Back"/>
+                            </figure>
+                            <div class="callto-info">
+                                <h2>MONEY BACK Guarantee!</h2>
+                                <p>Send within 30 days</p>
+                            </div>
+                        </div>
+                        <!-- Single Call-to Action End -->
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <!-- Single Call-to Action Start -->
+                        <div class="single-callto-action d-flex">
+                            <figure class="callto-thumb">
+                                <img src="assets/img/cog.png" alt="Guide"/>
+                            </figure>
+                            <div class="callto-info">
+                                <h2>SHOPPING GUIDE</h2>
+                                <p>Quis Eum Iure Reprehenderit</p>
+                            </div>
+                        </div>
+                        <!-- Single Call-to Action End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Call to Action End -->
+
+    <!-- Footer Follow Up Area Start -->
+    <div class="footer-followup-area">
+        <div class="ruby-container">
+            <div class="followup-wrapper">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <div class="follow-content-wrap">
+                            <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="logo"/></a>
+                            <p>Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum</p>
+
+                            <div class="footer-social-icons">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-pinterest"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                                <a href="#"><i class="fa fa-flickr"></i></a>
+                            </div>
+
+                            <a href="#"><img src="assets/img/payment.png" alt="Payment Method"/></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Follow Up Area End -->
+
+    <!-- Footer Image Gallery Area Start -->
+    <div class="footer-image-gallery">
+        <div class="ruby-container">
+            <div class="image-gallery-wrapper">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="imgage-gallery-carousel owl-carousel">
+                            <div class="gallery-item">
+                                <a href="#"><img src="assets/img/gallery-img-1.jpg" alt="Gallery"/></a>
+                            </div>
+                            <div class="gallery-item">
+                                <a href="#"><img src="assets/img/gallery-img-2.jpg" alt="Gallery"/></a>
+                            </div>
+                            <div class="gallery-item">
+                                <a href="#"><img src="assets/img/gallery-img-3.jpg" alt="Gallery"/></a>
+                            </div>
+                            <div class="gallery-item">
+                                <a href="#"><img src="assets/img/gallery-img-4.jpg" alt="Gallery"/></a>
+                            </div>
+                            <div class="gallery-item">
+                                <a href="#"><img src="assets/img/gallery-img-3.jpg" alt="Gallery"/></a>
+                            </div>
+                            <div class="gallery-item">
+                                <a href="#"><img src="assets/img/gallery-img-2.jpg" alt="Gallery"/></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Image Gallery Area End -->
+
+    <!-- Copyright Area Start -->
+    <div class="copyright-area">
+        <div class="ruby-container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="copyright-text">
+                        <p><a target="_blank" href="https://www.templateshub.net">Templates Hub</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright Area End -->
+
+</footer>
 <!-- Footer Area End -->
 
 
 <!-- Start All Modal Content -->
 <!--== Product Quick View Modal Area Wrap ==-->
-<?php include('./main/model_view.php'); ?>
+<div class="modal fade" id="quickView" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"><img src="assets/img/cancel.png" alt="Close" class="img-fluid"/></span>
+            </button>
+            <div class="modal-body">
+
+            </div>
+        </div>
+    </div>
+</div>
 <!--== Product Quick View Modal Area End ==-->
 <!-- End All Modal Content -->
 
@@ -1078,7 +1242,20 @@ $products= $productModel->showProduct();
 
 <!--=======================Javascript============================-->
 <script src="assets/js/main.js"></script>
-<?php include './main/src_js.php' ?>
+<!--=== Jquery Min Js ===-->
+<script src="admin-page/view/assets/js/jquery-3.6.0.min.js"></script>
+<!--=== Jquery Migrate Min Js ===-->
+<script src="assets/js/vendor/jquery-migrate-1.4.1.min.js"></script>
+<!--=== Popper Min Js ===-->
+<script src="assets/js/vendor/popper.min.js"></script>
+<!--=== Bootstrap Min Js ===-->
+<script src="assets/js/vendor/bootstrap.min.js"></script>
+<!--=== Plugins Min Js ===-->
+<script src="assets/js/plugins.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<!--=== Active Js ===-->
+<script src="assets/js/active.js"></script>
+<script src="assets/js/main.js"></script>
 </body>
 
 </html>
